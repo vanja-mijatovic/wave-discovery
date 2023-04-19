3fce477109ec7376f49a54eb6cb1487b18317fa0
Vanja Mijatovic <vmijatovic2000@gmail.com>
2023-04-19 22:01:20 +0200

    Added Eureka discovery server using Spring Boot project for microservices management

---
+++ wavediscovery/src/test/java/com/mijatovic/wavediscovery/WaveDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mijatovic.wavediscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaveDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
